9ea512c69d4d93dcfbd61510316a6e16c9650781
Wesonga06 <cindywesonga7@gmail.com>
2026-01-22 20:10:51 +0300

    cmt

--- database/migrations/2026_01_17_172852_create_goals_table.php
+++ database/migrations/2026_01_17_172852_create_goals_table.php
@@ -11,10 +11,14 @@ public function up(): void
     Schema::create('goals', function (Blueprint $table) {
         $table->id();
         $table->foreignId('user_id')->constrained()->onDelete('cascade');
-        $table->string('name');
+        $table->string('title');
         $table->decimal('target_amount', 15, 2);
         $table->decimal('current_amount', 15, 2)->default(0);
         $table->timestamps();
     });
+}
+public function down(): void
+{
+    Schema::dropIfExists('goals');
 }
 };
